4d0a802b32500bde6eb62393bd2d094a9295fc67
himaindla576 <himaindla576@gmail.com>
2026-04-14 16:35:57 +0530

    buliding html

--- IntroArrays.js
+++ IntroArrays.js
@@ -1,7 +1,39 @@
 document.getElementById("btnSubmit").addEventListener("click",function(){
     debugger;
-    var index=document.getElementById("txtIndex").value;
-    var shoppingapp=Number["myntra","ajio",'savana','flipkart','amazon']
-    document.getElementById("pResult").innerHTML=shoppingapp[index];
-}
-)
+    var index=Number(document.getElementById("txtIndex").value);
+    var shoppingApp=["myntra","ajio",'savana','flipkart','amazon'];
+    document.getElementById("pResult").innerHTML=shoppingApp[index];
+
+});
+
+
+
+  document.getElementById("btnFirstElement").addEventListener("click",function(){
+    debugger;
+
+    var index=Number(document.getElementById("txtIndex").value);
+     var shoppingApp=["myntra","ajio",'savana','flipkart','amazon'];
+     document.getElementById("pResult").innerHTML=shoppingApp[0];
+
+  });
+
+
+
+
+    document.getElementById("length").addEventListener("click",function(){
+        debugger;
+
+     var shoppingApp=["myntra","ajio",'savana','flipkart','amazon'];
+      document.getElementById("pResult").innerHTML=shoppingApp.length
+
+         });
+
+
+
+
+          document.getElementById("btnlastElement").addEventListener("click",function(){
+        debugger;
+
+     var shoppingApp=["myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','amazon',"myntra","ajio",'savana','flipkart','hima'];
+      document.getElementById("pResult").innerHTML=shoppingApp[shoppingApp.length-1];        
+     });
