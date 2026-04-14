54d165d04901fdde5dc12c9aa5bc349d205194f5
himaindla576 <himaindla576@gmail.com>
2026-04-14 13:30:03 +0530

    bug the mistakes

--- IntroArrays.js
+++ IntroArrays.js
@@ -1,7 +1,7 @@
 document.getElementById("btnSubmit").addEventListener("click",function(){
     debugger;
     var index=document.getElementById("txtIndex").value;
-    var shopingapp=["myntra","ajio",'savana','flipkart','amazon']
-    document.getElementById("pResult").innerHTML=Name[0]+" ,"+Name[1]+","
+    var shoppingapp=Number["myntra","ajio",'savana','flipkart','amazon']
+    document.getElementById("pResult").innerHTML=shoppingapp[index];
 }
 )
