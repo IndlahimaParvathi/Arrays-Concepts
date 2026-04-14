92d75687ba6991e76b098e7b71a51ab6a5b1f334
himaindla576 <himaindla576@gmail.com>
2026-04-14 12:59:00 +0530

    buliding js

---
+++ IntroArrays.js
@@ -0,0 +1,7 @@
+document.getElementById("btnSubmit").addEventListener("click",function(){
+    debugger;
+    var index=document.getElementById("txtIndex").value;
+    var shopingapp=["myntra","ajio",'savana','flipkart','amazon']
+    document.getElementById("pResult").innerHTML=Name[0]+" ,"+Name[1]+","
+}
+)
