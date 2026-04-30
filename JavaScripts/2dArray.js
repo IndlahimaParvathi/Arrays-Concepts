4182bcaa651c4917895ce9e40a5b43e362da8d54
himaindla576 <himaindla576@gmail.com>
2026-04-30 11:18:48 +0530

    buliding html

--- JavaScripts/2dArray.js
+++ JavaScripts/2dArray.js
@@ -1,8 +1,6 @@
-documentgetElementById("pResult").addEventListener("Click",function(){
+document.getElementById("btnArrayToArray").addEventListener("Click",function(){
     debugger;
     var andGate=[[0,0,0],[0,1,0],[1,0,0],[1,1,1]];
-    var inputIndex=document.getElementById("txtInputIndex").value
-
-    document.getElementById("pResult").innerHTML=andGate[1];
-
-    });
+    var inputIndex=Number(document.getElementById("txtInputIndex").value);
+ document.getElementById("pResult").innerHTML=andGate[0];
+ });
