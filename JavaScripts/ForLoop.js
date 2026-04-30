3084b6c93855c90ca13192336a9f1a6b307428df
himaindla576 <himaindla576@gmail.com>
2026-04-30 15:14:34 +0530

    even  odd js

--- JavaScripts/ForLoop.js
+++ JavaScripts/ForLoop.js
@@ -15,3 +15,33 @@ document.getElementById("btnSquare").addEventListener("click",function(){
     }
     
 });
+
+document.getElementById("btnNumberPrinter").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=0; i<=number; i++){
+        console.log(i)
+    }
+    
+});
+
+document.getElementById("btnEven").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=2; i<=number; i=i+2){
+        console.log(i)
+    }
+    
+});
+
+document.getElementById("btnOdd").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i=i+1){
+        console.log(i)
+    }
+    
+});
+
+
+
