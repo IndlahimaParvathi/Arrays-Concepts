c38130c024c522739d0539ae231baf94705ae050
himaindla576 <himaindla576@gmail.com>
2026-05-01 15:55:12 +0530

    writing js

--- JavaScripts/ForLoop.js
+++ JavaScripts/ForLoop.js
@@ -1,56 +1,78 @@
 document.getElementById("btnCude").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
-    for (i=0; i<=number; i++){
-        console.log(i*i*i)
+        var content='';
+ for (i=0; i<=number; i++){
+    content=content+"<p>"+(i*i*i)+"</P>" 
     }
-    
+     document.getElementById("pResult").innerHTML=content; 
 });
+
+
 
 document.getElementById("btnSquare").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
+    var content='';
     for (i=0; i<=number; i++){
-        console.log(i*i)
+       content=content+"<p>"+(i*i)+"</P>" 
     }
-    
+     document.getElementById("pResult").innerHTML=content;  
 });
+
+
+
 
 document.getElementById("btnNumberPrinter").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
+    var content='';
     for (i=0; i<=number; i++){
-        console.log(i)
+     content=content+"<p>"+i+"</P>"     
     }
-    
+       document.getElementById("pResult").innerHTML=content;
 });
+
+
+
 
 document.getElementById("btnEven").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
+        var content='';
     for (i=2; i<=number; i=i+2){
-        console.log(i)
+    content=content+"<p>"+i+"</P>"     
     }
-    
+     document.getElementById("pResult").innerHTML=content;
 });
+
+
+
 
 document.getElementById("btnOdd").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
+    var content='';
     for (i=1; i<=number; i=i+2){
-        console.log(i)
+        content=content+"<p>"+i+"</P>" 
     }
-    
+     document.getElementById("pResult").innerHTML=content;
 });
+
+
+
+
 
 document.getElementById("btnDecrease").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value);
+    var content="";
     for (i=100; i>=number; i--){
-        console.log(i)
+       content=content+"<p>"+i+"</P>"
     }
-    
+    document.getElementById("pResult").innerHTML=content; 
 });
+
 
 
 
