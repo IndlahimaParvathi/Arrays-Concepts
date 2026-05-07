71fe1c384afd285a20b5ab7d50b9e2cb506e3785
himaindla576 <himaindla576@gmail.com>
2026-05-07 22:30:05 +0530

    nand gate

--- JavaScripts/2dArray.js
+++ JavaScripts/2dArray.js
@@ -2,5 +2,5 @@ document.getElementById("btnArrayToArray").addEventListener("click",function(){
     debugger;
     var andGate=[[0,0,0],[0,1,0],[1,0,0],[1,1,1]];
     var inputIndex=Number(document.getElementById("txtInputIndex").value);
- document.getElementById("pResult").innerHTML=andGate[0];
+ document.getElementById("pResult").innerHTML=andGate[1];
  });
